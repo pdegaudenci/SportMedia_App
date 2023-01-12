4fa30675c3ae2ad7f8e4901132e83099540dbfab
Sebastian Degaudenci <pdegaudenci@hotmail.com>
2023-01-12 20:55:00 +0100

    Creado activity Home con menu lateral

--- app/src/main/java/com/example/sportsmedia/HomeActivity.java
+++ app/src/main/java/com/example/sportsmedia/HomeActivity.java
@@ -1,6 +1,11 @@
 package com.example.sportsmedia;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -17,7 +22,7 @@ public class HomeActivity extends AppCompatActivity {
     public static void setUserglobal(Usuario userglobal) {
         HomeActivity.userglobal = userglobal;
     }
-
+    DrawerLayout drawerLayout;
 
 
     @Override
@@ -26,10 +31,30 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         String text = getString(R.string.welcome, getUserglobal().getNombre());
-        System.out.println("ENTROOOOOO"+text);
-        user=(TextView) findViewById(R.id.user_home);
-        user.setText(text);
 
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.setTitle(text);
+        }
+        toggle.syncState();
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 
