7e031e3aeffd6aca5c06fce4438b4f1d390998a6
pdegaudenci@hotmail.com <pdegaudenci@hotmail.com>
2023-02-02 21:07:14 -0800

    Menu lateral con enlaces a fragments y fragment contenedor del actvity_main

--- app/src/main/java/com/example/sportsmedia/HomeActivity.java
+++ app/src/main/java/com/example/sportsmedia/HomeActivity.java
@@ -1,20 +1,28 @@
 package com.example.sportsmedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.example.sportsmedia.auxiliar.ActividadesFragment;
+import com.example.sportsmedia.auxiliar.HomeFragment;
+import com.example.sportsmedia.auxiliar.RedFragment;
 import com.example.sportsmedia.models.Usuario;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
     private static Usuario userglobal;
     private TextView user;
+    private NavigationView navigationView;
     public static Usuario getUserglobal() {
         return userglobal;
     }
@@ -46,8 +54,46 @@ public class HomeActivity extends AppCompatActivity {
             actionBar.setTitle(text);
         }
         toggle.syncState();
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        cargarListener();
 
     }
+
+    private void cargarListener(){
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                boolean fragmentTransaction = false;
+                System.out.println("ENTRA ");
+                switch (item.getItemId()){
+                    case R.id.nav_activities:
+                        fragment = new ActividadesFragment();
+                        fragmentTransaction = true;
+                        break;
+                    case R.id.nav_red:
+                        fragment = new RedFragment();
+                        fragmentTransaction = true;
+                        break;
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        fragmentTransaction = true;
+                        break;
+                }
+
+                if (fragmentTransaction){
+                    changeFragment (fragment , item );
+                    drawerLayout.closeDrawers();
+                }
+                return true;
+            }
+        });
+    }
+    /*
+   Metodo que visualiza / oculta el icono de apertura del menu lateral ,
+   en funcion de que el menu este desplegado o no
+     */
     @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -56,6 +102,23 @@ public class HomeActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+    /**
+     * Metodo para cambiar los fragment a visualizar , dentro de la actividad principal
+     * Recibe por parámetro, el fragment a cargar en la activity_home , en el elemento con id fragment
+     * @param fragment
+     * @param item
+     */
+    private void changeFragment( Fragment fragment, MenuItem item){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment , fragment)
+                .commit();
+        item.setChecked(true);
+        // Seteo titulo superior del fragment cargado
+        getSupportActionBar().setTitle(item.getTitle());
+    }
+
 
 
 }
